daee2da62f63ac3be917741332825264be88211d
SantaClauseMacros <theojcorsini@gmail.com>
2024-10-17 08:25:53 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import PCPartPicker from './components/PCPartPicker';
+import './styles.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/pc-part-picker" component={PCPartPicker} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
